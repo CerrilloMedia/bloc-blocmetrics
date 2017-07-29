b91e0bed884bed76fcbda41288eeaf5ac6a4b0f1
Armando Cerrillo <armando@cerrillomedia.org>
2017-07-29 04:27:58 +0000

    create API controller for registering application events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,36 +2,35 @@ class API::EventsController < ApplicationController
   
   skip_before_action :verify_authenticity_token
   
-  #Skip devise login-check
+  # Skip devise login credentials
   skip_before_action :authenticate_user!
   
   before_filter :set_access_control_headers
   
   # Set CORS ( Cross Origin Resource Scripting ) to allow for bypassing Rails CSRF check
-  
   def set_access_control_headers
     # '*' Allows for a request from ANY origin
-    header['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = '*'
     # permit the following request methods
-    header['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     # Allowed to declare type of data being sent through
-    header['Access-Control-Allow-Headers'] = 'Content-Type'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
     
   
   def create
+    
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     
     if registered_application.nil?
       render json: "Unregistered application", status: :unprocessable_entity
     else
       @event = registered_application.events.create(event_params)
-    end
-    
-    if @event.save
-      render json: @event, status: :created
-    else
-      render json: { errors: @event.errors }, status: :unprocessed_entity
+      if @event.save
+        render json: @event, status: :created
+      else
+        render json: { errors: @event.errors }, status: :unprocessed_entity
+      end
     end
     
   end
